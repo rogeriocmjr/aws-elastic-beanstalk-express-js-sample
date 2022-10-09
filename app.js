d64f5539a750a26291c5d0768030c72e37de1e37
Rogério Crespo Martins Júnior <rogerio.cmjr@gmail.com>
2022-10-09 16:21:49 -0300

    That's one small step for man, one giant leap for mankind.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('My first pipeline with AWS!'));
+app.get('/', (req, res) => res.send('To the infinity and beyond!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
